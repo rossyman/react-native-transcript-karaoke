52a68d63c4dd737572500693eca40c1f73cd4bab
Ross MacPhee <ross@getinflow.io>
2022-10-23 22:46:04 +0100

    [2.0.0]: Remove deprecated refs and update docs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
-import { Karaoke } from './components/Karaoke'
-
 export * from './models/progress-type'
 export * from './models/karaoke-props'
 export * from './utils/as-karaoke-map'
-export { Karaoke } from './components/Karaoke'
-
-const TranscriptKaraoke = Karaoke
-
-/**
- * @deprecated Please use the new nomenclature: `import { Karaoke } from 'react-native-transcript-karaoke'`
- */
-export default TranscriptKaraoke
+export * from './components/Karaoke'
